f16a396648b189c79ac9369c3d3e1be5336a2f21
albarqi19 <89564646+albarqi19@users.noreply.github.com>
2024-12-28 17:47:23 +0300

    تحديث إعدادات TypeScript و Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist/client',
+    emptyOutDir: true,
+    sourcemap: true
+  },
   server: {
-    port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
-        ws: true
       }
     }
   }
-});
+})
